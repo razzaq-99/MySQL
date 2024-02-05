af3368d803a3e8b7c2501ff03fcdadda57d334bc
abdul Razzaq <ar8937562@gmail.com>
2024-02-05 17:28:42 +0500

    SELECT in mysql

--- index.sql
+++ index.sql
@@ -1,1 +1,7 @@
 SELECT * FROM demo.customers_data;
+
+select FirstName,BirthDate from demo.customers_data;
+
+select FirstName,Gender,MaritalStatus from demo.customers_data;
+
+select Occupation from demo.customers_data
